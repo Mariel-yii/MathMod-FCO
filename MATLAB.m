fa00bc3958c4a88ceff824ffc41489e3de2d7058
Mariel-yii <liliesssye05@gmail.com>
2026-03-31 16:02:08 +0800

    Revise graph edges, weights, and output messages
    Updated edges and weights for the graph and modified the title and output for the shortest path.

--- MATLAB.m
+++ MATLAB.m
@@ -1,23 +1,24 @@
->> 
-% EDGES & WEIGHTS
-s = {'G1', 'AC', 'G2', 'CICT', 'CICT', 'ELib', 'CSSP', 'CSSP', 'G3', 'VH', 'FE'};
-t = {'AC', 'CICT', 'CICT', 'ELib', 'FH', 'CSSP', 'G4', 'FH', 'VH', 'FE', 'FH'};
-weights = [49, 88, 103, 149, 175, 45, 30, 45, 75, 85, 10];
+>> % EDGES
+s = {'G1', 'AC', 'CICT', 'CoEd', 'G2', 'COE', 'G3', 'VH', 'VH', 'G4', 'CSSP'};
+t = {'AC', 'CICT', 'CoEd', 'FH',   'COE', 'FH',  'VH', 'FH', 'FE', 'CSSP', 'FH'};
+% Weights of Edges
+weights = [58, 80, 92, 71, 35, 40, 73, 21, 21, 169, 18];
 
 % GRAPH
 G = graph(s, t, weights);
 figure('Color', [1 1 1]);
-p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', 2, 'MarkerSize', 7);
+p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', 2, 'MarkerSize', 10);
 layout(p, 'force');
-title('BulSU Pedestrian Network - Final Mathematical Model');
+title('BulSU Pedestrian Network Gate 1 to Federizo Hall');
 
 % Shortest Path from Gate 1 to Federizo Hall
 [path, d] = shortestpath(G, 'G1', 'FH');
 highlight(p, path, 'EdgeColor', 'r', 'LineWidth', 4);
 
-% RESULTS
-fprintf('Shortest Path Distance: %.2f meters\n', d);
-fprintf('Route: %s\n', strjoin(path, ' -> '));
-Shortest Path Distance: 312.00 meters
-Route: G1 -> AC -> CICT -> FH
+% Result (Highlighting path from Gate 1 to Federizo Hall)
+fprintf('Shortest Path from Gate 2 to FH: %.2f meters\n', d);
+fprintf('Path: %s\n', strjoin(path, ' -> '));
+
+Shortest Path from Gate 2 to FH: 301.00 meters
+Path: G1 -> AC -> CICT -> CoEd -> FH
 >> 
