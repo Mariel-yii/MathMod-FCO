1230a950e4352ca538a1e5f2e586d89368f9538b
Mariel Alvarado <liliesssye05@gmail.com>
2026-04-15 21:20:17 +0800

    Update Shortest_Distance_to_FH.m

--- src/Shortest_Distance_to_FH.m
+++ src/Shortest_Distance_to_FH.m
@@ -1,5 +1,35 @@
-bar([301 105 94 187])
-xticklabels({'G1','G2','G3','G4'})
-ylabel('Distance (m)')
-xlabel(‘Gates’)
-title('Shortest Distance to FH')
+# BulSu Network with distance
+G = Graph()
+
+# Add weighted edges (u, v, weight)
+edges = [
+    ('G1', 'AC', 58),
+    ('AC', 'CICT', 80),
+    ('CICT', 'CoEd', 92),
+    ('CoEd', 'FH', 71),
+    
+    ('G2', 'COE', 35),
+    ('COE', 'FH', 70),
+    
+    ('G3', 'VH', 73),
+    ('VH', 'FH', 21),
+    
+    ('G4', 'CSSP', 169),
+    ('CSSP', 'FH', 18)
+]
+
+G.add_edges(edges)
+
+# Sources and destination
+sources = ['G1', 'G2', 'G3', 'G4']
+target = 'FH'
+
+print("{:<6} | {:<40} | {:<8}".format("Source","Shortest Path","Distance"))
+print("-"*70)
+
+# Compute shortest paths
+for s in sources:
+    path = G.shortest_path(s, target, by_weight=True)
+    dist = G.distance(s, target, by_weight=True)
+    print("{:<6} | {:<40} | {:<8}".format(s, str(path), dist))
+G.show()
